10d6720dd5f13963b5b9a7500f782b3ca6454bed
sivadharanisandula <154986893+sivadharanisandula@users.noreply.github.com>
2025-07-30 06:19:39 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import QuizPage from "./QuizPage";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import QuizPage from './QuizPage';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QuizPage />
   </React.StrictMode>
